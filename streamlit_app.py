9f01cc15f2cdbf3321409a5a8b7009e098dc66c6
Romain Lapostolle <rlapostolle@gmail.com>
2025-05-27 17:58:05 +0200

    improve score computation

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,18 @@ if 'ban_units' not in st.session_state:
 if 'available_units' not in st.session_state:
     st.session_state.available_units = []
 
-unit_defs = data["characters"]
+unit_defs = dict([(u["name"], u) for u in data["characters"] ])
 
-for unit in unit_defs:
+for unit_id, unit in unit_defs.items():
     unit.setdefault("do_counter", [])
-    for other in unit_defs:
+    for other_id, other in unit_defs.items():
         for other_counter in other["counters"]:
-            if other_counter["name"] == unit["name"]:
-                unit["do_counter"].append(other)
+            if other_counter["name"] == unit_id:
+                print (f"Adding {unit_id} to {other_id}'s do_counter")
+                unit["do_counter"].append({"name": other["name"], "rating": other_counter["rating"]})
 
-for unit in unit_defs:
+for unit_id, unit in unit_defs.items():
     cols = st.columns([0.2, 0.8])
-    unit_id = unit["name"]
     img_path = os.path.join(image_folder, unit["img"])
 
     cols[0].image(img_path, width=100)
@@ -79,40 +79,86 @@ for unit in unit_defs:
             st.session_state.available_units.remove(unit_id)
 
 ordered_units = []
-for unit in unit_defs:
+unit_scores = {}
+for unit_id, unit in unit_defs.items():
     score = unit["rating"]
     # check if the unit is in own_units or oppo_units
-    for oppo_avail_unit_id in st.session_state.oppo_units + st.session_state.available_units:
-        counters = unit["counters"] if unit["counters"] else []
-        found = next(filter(lambda x : x["name"] == oppo_avail_unit_id, counters), None)
-        if found != None:
-            score -= found["rating"]
-    for oppo_unit_id in st.session_state.oppo_units:
-        do_counter = unit["do_counter"] if unit["do_counter"] else []
-        found = next(filter(lambda x : x["name"] == oppo_unit_id, do_counter), None)
-        if found != None:
-            score += found["rating"]
+    countered_by = unit["counters"] if unit["counters"] else []
+    if unit_id in st.session_state.available_units:
+        for oppo_avail_unit_id in st.session_state.oppo_units + (st.session_state.available_units if len(st.session_state.oppo_units) < 4 else []):
+            found = next(filter(lambda x : x["name"] == oppo_avail_unit_id, countered_by), None)
+            if found != None:
+                score -= found["rating"]
+    if unit_id in st.session_state.own_units:
+        for oppo_unit_id in st.session_state.oppo_units:
+            # only remove the score if if got countered by the opponent's unit
+            #found = next(filter(lambda x : x["name"] == oppo_unit_id, do_counter), None)
+            #if found != None:
+            #    score += found["rating"]
+            found = next(filter(lambda x : x["name"] == oppo_unit_id, countered_by), None)
+            if found != None:
+                score -= found["rating"]
+    if unit_id in st.session_state.oppo_units:
+        for own_unit_id in st.session_state.own_units:
+            # only remove the score if it got countered by our unit
+            #found = next(filter(lambda x : x["name"] == own_unit_id, do_counter), None)
+            #if found != None:
+            #    score += found["rating"]
+            found = next(filter(lambda x : x["name"] == own_unit_id, countered_by), None)
+            if found != None:
+                score -= found["rating"]
+
     if score < min(20, unit["rating"]):
         score = min(20, unit["rating"])
     ordered_units.append((unit, score))
+    unit_scores[unit_id] = score
 
+for unit_id, unit in unit_defs.items():
+    if unit_id in st.session_state.available_units:
+        do_counter = unit["do_counter"] if unit["do_counter"] else []
+        for oppo_avail_unit_id in st.session_state.oppo_units:
+            found = next(filter(lambda x : x["name"] == oppo_avail_unit_id, do_counter), None)
+            if found != None:
+                found_score = unit_scores[found["name"]]
+                diff = found_score - max(20, found_score - found["rating"])
+                unit_scores[unit_id] += min(found["rating"], diff)
+                ordered_units = list(filter(lambda x: x[0]["name"] != unit_id, ordered_units))
+                ordered_units.append((unit, unit_scores[unit_id]))
 
 ordered_units.sort(key=lambda x: x[1], reverse=True)
 
 with st.sidebar:
+
+    st.markdown("### Own Team Score")
+    own_score = sum(unit_scores[unit] for unit in st.session_state.own_units)
+    st.text(f"{own_score:.0f}")
+    st.markdown("### Opponent Team Score")
+    oppo_score = sum(unit_scores[unit] for unit in st.session_state.oppo_units)
+    st.text(f"{oppo_score:.0f}")
+
+    st.markdown("### Units Overall Score")
     for (unit, score) in ordered_units:
         unit_id = unit["name"]
+        if unit_id in st.session_state.ban_units:
+            continue
         img_path = os.path.join(image_folder, unit["img"])
         
         containers = st.columns([0.3, 0.4, 0.3])
         containers[0].image(img_path, width=50)
         if unit_id in st.session_state.own_units:
-            color = "green"
-        if unit_id in st.session_state.ban_units:
-            color = "red"
+            containers[1].markdown(':green[' + unit["name"] + ']')
         if unit_id in st.session_state.oppo_units:
-            color = "violet"
+            containers[1].markdown(':red[' + unit["name"] + ']')
         if unit_id in st.session_state.available_units:
-            color = None
-        containers[1].markdown(':' + color + '[' + unit["name"] + ']' if color else unit["name"])
+            containers[1].markdown(unit["name"])
         containers[2].text(f"Score: {score:.02f}")
+
+    st.markdown("### Banned Units")
+    for (unit, score) in ordered_units:
+        unit_id = unit["name"]
+        if unit_id not in st.session_state.ban_units:
+            continue
+        containers = st.columns([0.3, 0.4, 0.3])
+        img_path = os.path.join(image_folder, unit["img"])
+        containers[0].image(img_path, width=50)
+        containers[1].markdown('~~' + unit["name"] + '~~')
